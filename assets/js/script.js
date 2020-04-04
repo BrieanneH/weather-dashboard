7a734b28d0358ad75daf4ab5482a2a2790320bef
BrieanneH <howard.brieanne@gmail.com>
2020-04-03 23:08:08 -0500

    html adds and pseudo code

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+
+    setInterval(function(){
+        //display date
+    })
+
+
+    //city info
+
+
+    //onclick function for getting city
+
+
+    var city =
+    var queryURL=""
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    //gather temp info (convert) -city
+    
+    
+    
+    
+    //call ids and add values -city
+
+
+
+
+    //save to local storage
+
+
+
+    //get method - forecast
+
+
+    //call ids and values -forecast
+
+});
